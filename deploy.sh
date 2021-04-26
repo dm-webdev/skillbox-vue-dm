119c1d0f31f30f336b7aea3defba342efe949cea
Denis Mironenko <denis.mironenko@mail.ru>
2021-04-26 20:34:39 +0300

    add deploy
    add deploy

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+#остановить публикацю при ошибках
+set -e
+
+#сборка приложения
+npm run build
+
+#переход в каталог сборки
+cd dist
+
+#инициализация репозитория и загрузка кода в GitHub
+git init
+git add -add
+git commit -m 'deploy'
+
+git push -f git@github.com:dm-webdev/vue-app.git master:gh-pages
+
+cd -
